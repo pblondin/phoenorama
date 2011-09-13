e09d0d813ba278220c470885d16bc58d1b50cf51
Philippe Blondin <pblondin@phoenorama.org>
2011-09-12 21:02:30 -0300

    Edited project/tools/openvas.py via GitHub

--- project/tools/openvas.py
+++ project/tools/openvas.py
@@ -35,7 +35,7 @@ class Openvas(threading.Thread):
     Threaded OpenVAS scan
        
     Example: 
-        OpenVAS-Client -T nbe -qx 127.0.0.1 9390 root courge990. /root/openvas/target /var/www/openvas/results.nbe
+        OpenVAS-Client -T nbe -qx 127.0.0.1 9390 <user> <pass> /root/openvas/target /var/www/openvas/results.nbe
     '''
     HOST = '127.0.0.1'
     PORT = 9390
