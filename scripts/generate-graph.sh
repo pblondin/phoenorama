91b6d5eda9f1fc0f16982933b2e706ba323350f2
Philippe Blondin <pblondin@phoenorama.org>
2011-09-16 11:46:35 -0400

    script to generate a graph from django model view

---
+++ scripts/generate-graph.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+[[ -n "$1" ]] || { echo "Usage: $0 <output>"; exit 0 ; }
+[[ ! -f "$1.jpg" ]] || { echo "File '$1.jpg' already exists"; exit 0; }
+
+APPS="project account management infosec admin"
+OUTPUT=$1
+
+python manage.py graph_models $APPS -l dot > $OUTPUT.dot
+dot -T jpg $OUTPUT.dot > $OUTPUT.jpg
+rm -f $OUTPUT.dot
+
